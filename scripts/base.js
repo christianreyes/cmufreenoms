5f7d41088706422e577df87895ef13b857cacd98
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-11-09 22:45:27 -0500

    client-side validation for report form

--- scripts/base.js
+++ scripts/base.js
@@ -1,59 +1,64 @@
 // Handler for .ready() called.
 $(function(){	
 	highlight_current_tab();
-		
-	var descriptions = $('#reports .description');
-	descriptions.each(function(index, description){
-		description = $(description);
-		var description_words = description.text().split(" ");
 	
-		for(var i=0 ; i < description_words.length; i++){
-			var hash_word = description_words[i];
-			var match_data = hash_word.match(/#([\w\d]*)/);
-			if(match_data){
-				var word = match_data[1];
-				var tag_link = $('<a class="tagged_food"></a>');
-				tag_link.attr("href", "/find/" + word); 
-				tag_link.text(hash_word);
-				description_words[i] = tag_link[0].outerHTML;
-				
+	if($('.find').length > 0) {
+		var descriptions = $('#reports .description');
+		descriptions.each(function(index, description){
+			description = $(description);
+			var description_words = description.text().split(" ");
+
+			for(var i=0 ; i < description_words.length; i++){
+				var hash_word = description_words[i];
+				var match_data = hash_word.match(/#([\w\d]*)/);
+				if(match_data){
+					var word = match_data[1];
+					var tag_link = $('<a class="tagged_food"></a>');
+					tag_link.attr("href", "/find/" + word); 
+					tag_link.text(hash_word);
+					description_words[i] = tag_link[0].outerHTML;
+
+				}
 			}
-		}
-		description.html(description_words.join(" "));
-		$('#reports .description .tagged_food').click(function(e){
+			description.html(description_words.join(" "));
+			$('#reports .description .tagged_food').click(function(e){
+				searchKeyPressed(e, $(this).attr('href').match(/find\/(.*)/)[1]);
+				setSearchText();
+				return false;
+			});
+		});
+
+		$('#reports .location').click(function(e){
 			searchKeyPressed(e, $(this).attr('href').match(/find\/(.*)/)[1]);
 			setSearchText();
 			return false;
 		});
-	});
-	
-	$('#reports .location').click(function(e){
-		searchKeyPressed(e, $(this).attr('href').match(/find\/(.*)/)[1]);
+
+		$('#find_search_box').bind("keydown", function(e){
+			element = this;
+			setTimeout(function(){ searchKeyPressed(e,$(element).val()); }, 1);
+		});
+
+		$('#num_hours').text(2);
+
+		$('#slider').slider({
+			value: 2,
+			min: 1,
+			max: 5,
+			step: 1,
+			slide: function(event, ui){
+				$('#num_hours').text(ui.value);
+				filterReports($('#find_search_box').val());
+			}
+		});		
+		
 		setSearchText();
-		return false;
-	});
-	
-	$('#find_search_box').bind("keydown", function(e){
-		element = this;
-		setTimeout(function(){ searchKeyPressed(e,$(element).val()); }, 1);
-	});
-	
-	$('#num_hours').text(2);
-	
-	$('#slider').slider({
-		value: 2,
-		min: 1,
-		max: 5,
-		step: 1,
-		slide: function(event, ui){
-			$('#num_hours').text(ui.value);
-			filterReports($('#find_search_box').val());
-		}
-	});
-	
-	
-	setSearchText();
-	filterReports($('#find_search_box').val())
+		filterReports($('#find_search_box').val())
+	}
+
+	if($('.report').length > 0) {
+		setvalidation();
+	}
 });
 
 function displayNoticeIfNone(){
@@ -143,4 +148,72 @@ function highlight_current_tab(){
 		$('#menu li:nth-child(3)').css("background-color", "#eee");
 		$('#menu li:nth-child(3)').css("border-bottom", "3px solid #bbb");
 	}
+}
+
+/***************************/
+//@Author: Adrian "yEnS" Mato Gondelle & Ivan Guardado Castro
+//@website: www.yensdesign.com
+//@email: yensamg@gmail.com
+//@license: Feel free to use it, but keep this credits please!
+// Modified by Christian Reyes					
+/***************************/
+
+function setvalidation(){
+	var form = $(".report_form");
+    var description = $("#description");
+    var descriptionInfo = $("#descriptionInfo");
+    var location = $("#location");
+    var locationInfo = $("#locationInfo");
+
+    //On blur
+    description.blur(validateDescription);
+    location.blur(validateLocation);
+	
+	//On keydown
+	description.keyup(validateDescription);
+    location.keyup(validateLocation);
+	
+    //On Submitting
+    form.submit(function () {
+        if (validateDescription() &
+            validateLocation())
+            return true
+        else
+            return false;
+    });
+
+    //validation functions
+    function validateDescription() {
+        //if it's NOT valid
+        if (description.val().length < 1) {
+            description.addClass("error");
+            descriptionInfo.text("Required");
+            descriptionInfo.addClass("error");
+            return false;
+        }
+        //if it's valid
+        else {
+            description.removeClass("error");
+            descriptionInfo.text("");
+            descriptionInfo.removeClass("error");
+            return true;
+        }
+    }
+
+    function validateLocation() {
+        //if it's NOT valid
+        if (location.val().length < 1) {
+            location.addClass("error");
+            locationInfo.text("Required");
+            locationInfo.addClass("error");
+            return false;
+        }
+        //if it's valid
+        else {
+            location.removeClass("error");
+            locationInfo.text("");
+            locationInfo.removeClass("error");
+            return true;
+        }
+    }
 }
